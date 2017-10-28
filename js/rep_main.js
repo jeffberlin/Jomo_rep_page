2e5e30d846af607e97c552ec391f5ce2bffea769
Jeff Berlin <jeff@jbrally.com>
2017-10-27 22:01:34 -0400

    Added OS detection

--- js/rep_main.js
+++ js/rep_main.js
@@ -1,6 +1,17 @@
+// OS detection
+if ( /Android|Opera Mini/i.test(navigator.userAgent) ) {
+    document.getElementById("ios-img").style.display = 'none';
+}
+
+if ( /iPhone|iPad|iPod/i.test(navigator.userAgent) ) {
+    document.getElementById("play-img").style.display = 'none';
+    document.getElementById("ios-img").style.marginRight = '0';
+}
+
 // 'Learn More' scroll button functions
 $(".learn-button").click(function() {
     $('html,body').animate({
         scrollTop: $(".instructions").offset().top
     }, 'slow')
 })
+
